e48c8b6741d48a315a03f8a3a1f756573866feda
Doniben <doniben@esperanto.co>
2020-12-12 12:48:13 -0500

    [FEATURE] structured project according to the Domain-oriented layered architecture

--- src/main/java/com/esperantomarket/web/controller/HolaMundocontroller.java
+++ src/main/java/com/esperantomarket/web/controller/HolaMundocontroller.java
@@ -1,4 +1,4 @@
-package com.esperantomarket;
+package com.esperantomarket.web.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
