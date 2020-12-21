5b43375ada495583d2e5d2fadab6d64624efa1a5
Doniben <doniben@esperanto.co>
2020-12-21 11:55:25 -0500

    Orientando API al Dominio con MapStruct

---
+++ src/main/java/com/esperantomarket/persistence/mapper/CategoryMapper.java
@@ -0,0 +1,21 @@
+package com.esperantomarket.persistence.mapper;
+import com.esperantomarket.domain.Category;
+import com.esperantomarket.persistence.entity.Categoria;
+import org.mapstruct.InheritInverseConfiguration;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
+
+@Mapper(componentModel = "spring")
+public interface  CategoryMapper {
+    @Mappings({
+            @Mapping(source = "idCategoria", target = "categoryId")
+            @Mapping(source = "descripcion", target = "category")
+            @Mapping(source = "estado", target = "active")
+    })
+    Category toCategory(Categoria categoria);
+
+    @InheritInverseConfiguration
+    @Mapping(target = "productos", ignore = true)
+    Categoria toCagoria(Category category)
+}
